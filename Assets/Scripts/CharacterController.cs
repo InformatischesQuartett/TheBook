afdd168c3a7a867d8c04eab2d35d7274ed70b558
Alexander Scheurer <mail@aspepex.net>
2015-04-11 01:53:07 +0200

    First steps for config, character controller and input
    + project settings

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour
+{
+
+    private float speed;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    speed = Config.CharacterWalkSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    this.transform.Translate(Input.GetAxis("Horizontal") * speed, 0, 0);
+
+	    if (Input.GetAxis("Submit") != 0)
+	    {
+	        Debug.Log("Submit");
+	    }
+	    if (Input.GetAxis("Cancel") != 0)
+	    {
+	        Debug.Log("Cancel");
+	    }
+        Debug.Log(Input.GetAxis("Mouse X"));
+	}
+}
---
+++ Assets/Scripts/Config.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+using UnityEngine;
+
+public static class Config
+{
+    private static readonly string _configPath = Application.streamingAssetsPath;
+    public static float CharacterWalkSpeed;
+
+    static Config()
+    {
+        var configContent = File.ReadAllText(_configPath + "/config.json");
+        var conf = JsonConvert.DeserializeObject<ConfigSet>(configContent);
+
+        CharacterWalkSpeed = conf.CharacterWalkSpeed;
+    }
+}
+
+internal struct ConfigSet
+{
+    public float CharacterWalkSpeed;
+}
