cf06fbe0a573f2dcc273ee86e38b4f6ceb7a6630
Alexander Scheurer <mail@aspepex.net>
2015-04-11 22:16:39 +0200

    Cursor for map and start screen

---
+++ Assets/Scripts/StartScreenStuff.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScreenStuff : MonoBehaviour {
+
+	// Use this for initialization
+    void Start()
+    {
+        Cursor.SetCursor(Config.Cursor, new Vector2(17, 9), CursorMode.Auto);
+    }
+}
