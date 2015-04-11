17d2a428f8e3acb5d30aa444648c14e03079c520
Alexander Scheurer <mail@aspepex.net>
2015-04-11 14:30:40 +0200

    StartScreen Dummy

---
+++ Assets/Scripts/GUIFunctions.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIFunctions : MonoBehaviour {
+
+    public void StartGame()
+    {
+        Application.LoadLevel("Alex");
+    }
+
+    public void ShowCredits()
+    {
+
+    }
+}
