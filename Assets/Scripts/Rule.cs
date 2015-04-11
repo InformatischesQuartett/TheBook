0ae95904eb18112971d29dd6ef53f36d23362564
Sarah Haefele <sarah.haefele@hs-furtwangen.de>
2015-04-11 11:17:58 +0200

    rules file and first book steps

---
+++ Assets/Scripts/Rule.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+/**
+ * This class represents a rule in the book.
+ **/
+public class Rule
+{
+
+    private String _rule;
+    private bool _active;
+   /**
+    * Constructor
+    **/
+    Rule(String rule)
+    {
+        _rule = rule;
+        _active = false;
+    }
+
+    /**
+    * Getter Rule
+    **/
+    public String getRule()
+    {
+        return _rule;
+    }
+
+    /**
+    * Setter State
+    **/
+    public void setActive(bool state)
+    {
+        _active = state;
+    }
+	
+}
