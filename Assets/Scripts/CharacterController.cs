54d9b28cd2f74631b73bb0421ba3d763185f69bc
Fabian Gärtner <fabian.gaertner@hs-furtwangen.de>
2015-04-11 22:48:39 +0200

    No more walking out of the scene

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -18,33 +18,7 @@ public class CharacterController : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-	    this.transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, 0);
-
-        if (Input.GetAxis("Horizontal") != 0)
-            this.GetComponent<Animator>().SetBool("Walk", true);
-        else
-            this.GetComponent<Animator>().SetBool("Walk", false);
-
-        if (Input.GetAxis("Horizontal") < 0)
-            this.transform.localScale = new Vector3(-0.29f, 0.29f, 0.29f);
-        if (Input.GetAxis("Horizontal") > 0)
-            this.transform.localScale = new Vector3(0.29f, 0.29f, 0.29f);
-
-	    if (Input.GetAxis("Submit") != 0)
-	    {
-	        Debug.Log("Submit");
-	    }
-	    if (Input.GetAxis("Cancel") != 0)
-	    {
-	        Debug.Log("Cancel");
-	    }
-
-		positionCam ();
-	}
-
-	void positionCam()
-	{
-		var script = townController.GetComponent<TownView>();
+        var script = townController.GetComponent<TownView>();
 		var camPos = camera.transform.position;
 		var newPos = new Vector3 (this.transform.position.x, camera.transform.position.y, camPos.z);
 
@@ -53,8 +27,37 @@ public class CharacterController : MonoBehaviour
 		var rightBaseScreen = Camera.main.WorldToScreenPoint (script.Town_Base.transform.position + script.GetWidth (script.Town_Base) / 2 * Vector3.right);
 		var camWorldWidth = Camera.main.ScreenToWorldPoint (new Vector3(leftBaseScreen.x, leftBaseScreen.y, 0));
 
-		if ((leftBaseScreen.x < 0 || newPos.x > camera.transform.position.x) && (rightBaseScreen.x > Screen.width ||  newPos.x < GetComponent<Camera>().transform.position.x)) {
-			camera.transform.position = new Vector3(this.transform.position.x, camera.transform.position.y, camPos.z);
-		}
+        var allowWalk = (Input.GetAxis("Horizontal") < 0 && leftBaseScreen.x < 0);
+        allowWalk |= (Input.GetAxis("Horizontal") > 0 && rightBaseScreen.x > Screen.width);
+        
+        if (allowWalk)
+        {
+            this.transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, 0);
+
+            if (Input.GetAxis("Horizontal") != 0)
+                this.GetComponent<Animator>().SetBool("Walk", true);
+            else
+                this.GetComponent<Animator>().SetBool("Walk", false);
+
+            var scale = this.transform.localScale.y;
+
+            if (Input.GetAxis("Horizontal") < 0)
+                this.transform.localScale = new Vector3(-scale, scale, scale);
+            if (Input.GetAxis("Horizontal") > 0)
+                this.transform.localScale = new Vector3(scale, scale, scale);
+
+            camera.transform.position = new Vector3(this.transform.position.x, camera.transform.position.y, camPos.z);
+        } else
+            this.GetComponent<Animator>().SetBool("Walk", false);
+
+	    if (Input.GetAxis("Submit") != 0)
+	    {
+	        Debug.Log("Submit");
+	    }
+
+	    if (Input.GetAxis("Cancel") != 0)
+	    {
+	        Debug.Log("Cancel");
+	    }
 	}
 }
