fe45791376b7187a207cd04b7b3ed98af225d8f7
Alexander Scheurer <mail@aspepex.net>
2015-05-29 19:12:10 +0200

    Pixelgame YAY

---
+++ Assets/Scripts/Pixelize.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof (Camera))]
+public class Pixelize : MonoBehaviour
+{
+    public int horizontalResolution = 640;
+    public int verticalResolution = 480;
+    public bool interpolated;
+
+    public void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        horizontalResolution = Mathf.Clamp(horizontalResolution, 1, 2048);
+        verticalResolution = Mathf.Clamp(verticalResolution, 1, 2048);
+
+        var scaled = RenderTexture.GetTemporary(horizontalResolution, verticalResolution);
+
+        if (interpolated)
+        {
+            scaled.filterMode = FilterMode.Bilinear;
+        }
+        else
+        {
+            scaled.filterMode = FilterMode.Point;
+        }
+
+        Graphics.Blit(src, scaled);
+        Graphics.Blit(scaled, dest);
+        RenderTexture.ReleaseTemporary(scaled);
+    }
+}
